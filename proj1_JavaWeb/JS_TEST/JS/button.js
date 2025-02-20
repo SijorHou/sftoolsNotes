a844c81e573c8f6fcb3f4e95d7e61d75ec80a8a0
SijorHou <sijorhou@163.com>
2025-02-20 12:05:12 +0800

    js

---
+++ proj1_JavaWeb/JS_TEST/JS/button.js
@@ -0,0 +1,5 @@
+function suprised() {
+    // 弹窗提示
+    // alert("Hello, this is SijorHou, please keep on moving, don't afraid and hesitate.\nJust chasing your dream!!")
+    alert("FUCKING LIFE HAS NO FUCKING SUPRISE !!!")
+}
